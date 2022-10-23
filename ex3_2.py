4e9e00cb41556d631a641e721d0214e6ae3f0e07
Anna Carolina <70608263+anna-garcia@users.noreply.github.com>
2022-10-23 19:21:37 -0300

    Add files via upload

---
+++ ex3_2.py
@@ -0,0 +1,8 @@
+#Faça um programa que leia quatro notas, mostre as notas E a média na tela.
+
+n1 = float(input("Digite a primeira nota:"))
+n2 = float(input("Digite a segunda nota:"))
+n3 = float(input("Digite a terceira nota:"))
+n4 = float(input("Digite a quarta nota:"))
+m = (n1 + n2 +n3 + n4)/4
+print('1a)', n1,'2a)', n2,'3a)', n3,'4a)', n4,'média:', m )
